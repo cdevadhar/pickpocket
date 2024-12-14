ad0c309e8449a9859e3b2d110c0509727f50684d
cdevadhar <cdevadhar@gmail.com>
2024-12-13 21:29:43 -0800

    basic extension (gets lines)

---
+++ getLines.js
@@ -0,0 +1,14 @@
+
+
+const mutationObserver = new MutationObserver(() => {
+    const picks = document.querySelectorAll('li#test-projection-li')
+    for (const pick of picks) {
+        const name = pick.querySelector('h3').innerHTML;
+        const line = pick.querySelector('.heading-md').children[0].innerHTML;
+        const statType = pick.querySelector('.break-words').innerHTML;
+        console.log(name+" "+line+" "+statType);
+    }
+})
+
+console.log("pickpocket active");
+mutationObserver.observe(document.body, { childList: true, subtree: true })
