af981c7b89ff07ebb9f6ead4de4595e92063e00a
stewartkwokca <stewart.kwok1@gmail.com>
2024-12-17 16:06:00 -0800

    add selected projection scraping

--- getLines.js
+++ getLines.js
@@ -1,5 +1,3 @@
-
-
 const mutationObserver = new MutationObserver(() => {
     const picks = document.querySelectorAll('li#test-projection-li')
     for (const pick of picks) {
@@ -7,6 +5,15 @@ const mutationObserver = new MutationObserver(() => {
         const line = pick.querySelector('.heading-md').children[0].innerHTML;
         const statType = pick.querySelector('.break-words').innerHTML;
         console.log(name+" "+line+" "+statType);
+    }
+
+    console.log("Picked:");
+    const picked = document.querySelectorAll('li.entry-prediction .player')
+    for (const pick of picked) {
+        const name = pick.querySelector('h3').innerHTML;
+        const projection = pick.querySelector('.projected-score>.score').textContent;
+        const statType = pick.querySelectorAll('.projected-score>div')[1].innerHTML;
+        console.log(name+" "+projection + " "+statType);
     }
 })
 
