120ff94aa95853950e614dd7be577aa50d54b88e
stewartkwokca <stewart.kwok1@gmail.com>
2024-12-27 10:14:45 -0800

    fix cors issue

--- backend/server.py
+++ backend/server.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": ["https://app.prizepicks.com"]}})
 
 def has_accent(word):
     normalized = unicodedata.normalize('NFD', word)
