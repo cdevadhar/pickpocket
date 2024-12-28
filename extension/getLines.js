8268ba2572215e1229852ad6a68c2b355636bff6
stewartkwokca <stewart.kwok1@gmail.com>
2024-12-27 21:17:20 -0800

    add break even rate frontend

--- extension/getLines.js
+++ extension/getLines.js
@@ -56,6 +56,37 @@ const mutationObserver = new MutationObserver(() => {
                 picked[i].append(proj);
             }
         }
+
+        let payoutOdds = payoutArea.querySelectorAll(".payout-odds");
+        for (const po of payoutOdds) {
+            po.remove();
+        }
+
+        let winrate = 0;
+        const payoutsElements = selectedPayout.querySelectorAll("div.payout");
+        for (let i = 0; i < payoutsElements.length; i++) {
+            const odds = document.createElement("div");
+            const displayOdds = Math.round((data['payoutodds'][i] + Number.EPSILON) * 10000) / 100;
+            odds.textContent = `(${displayOdds}%)`;
+            odds.classList.add("payout-odds");
+            odds.style.borderRadius = "3px";
+            odds.style.padding = "3px 2px";
+            odds.style.backgroundColor='#6eff00';
+            odds.style.color = 'BLACK';
+            odds.style.fontSize = 'small';
+            odds.style.fontWeight = "bold"
+            payoutsElements[i].append(odds);
+
+            if (payouts[i] >= 1){
+                winrate += data['payoutodds'][i];
+            }
+        }
+
+        const analytics = document.createElement("div");
+        analytics.style.display = "flex";
+        analytics.style.padding = "0px 5px";
+        payoutArea.append(analytics);
+
         let ev = payoutArea.querySelector(".expected-value");
         if (ev) ev.remove();
         ev = document.createElement("div");
@@ -65,6 +96,7 @@ const mutationObserver = new MutationObserver(() => {
         ev.style.padding = "10px"; 
         ev.style.border = 'medium solid BLACK';
         ev.style.borderRadius = "10px";
+        ev.style.flex = 1;
         if (displayEV < 1) {
             ev.style.backgroundColor='#e43245';
             ev.style.color = '#f0f0f7';
@@ -77,25 +109,31 @@ const mutationObserver = new MutationObserver(() => {
             ev.style.color = 'BLACK';
         }
         // console.log(data["payoutodds"]);
-        payoutArea.append(ev);
+        analytics.append(ev);
 
-        let payoutOdds = payoutArea.querySelectorAll(".payout-odds");
-        for (const po of payoutOdds) {
-            po.remove();
+        let wr = payoutArea.querySelector(".win-rate");
+        if (wr) wr.remove();
+        wr = document.createElement("div");
+        const displayWR = Math.round((winrate + Number.EPSILON) * 10000) / 100;
+        wr.textContent = `Break-Even Rate: ${displayWR}%`;
+        wr.classList.add("win-rate");
+        wr.style.padding = "10px"; 
+        wr.style.border = 'medium solid BLACK';
+        wr.style.borderRadius = "10px";
+        wr.style.flex = 1;
+        if (displayWR < 50) {
+            wr.style.backgroundColor='#e43245';
+            wr.style.color = '#f0f0f7';
+        } else if (displayWR < 60) {
+            wr.style.backgroundColor='#ffbb33';
+            wr.style.color = 'BLACK';
         }
-
-        const payoutsElements = selectedPayout.querySelectorAll("div.payout");
-        for (let i = 0; i < payoutsElements.length; i++) {
-            const odds = document.createElement("div");
-            const displayOdds = Math.round((data['payoutodds'][i] + Number.EPSILON) * 10000) / 100;
-            odds.textContent = `(${displayOdds}%)`;
-            odds.classList.add("payout-odds");
-            odds.style.borderRadius = "3px";
-            odds.style.padding = "5px";
-            odds.style.backgroundColor='#6eff00';
-            odds.style.color = 'BLACK';
-            payoutsElements[i].append(odds);
+        else {
+            wr.style.backgroundColor='#6eff00';
+            wr.style.color = 'BLACK';
         }
+        // console.log(data["payoutodds"]);
+        analytics.append(wr);
     })
     prevPicked = picked;
     prevPayout = selectedPayout;
