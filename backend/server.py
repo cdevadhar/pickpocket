eb219b52a8105c606defea46acc86d3fbb9b0762
stewartkwokca <stewart.kwok1@gmail.com>
2024-12-31 15:35:35 -0800

    unrestrict cors for dev for backend

--- backend/server.py
+++ backend/server.py
@@ -9,7 +9,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://app.prizepicks.com"}})
+CORS(app)
 
 lastInjuryUpdate = None
 injuries = {}
